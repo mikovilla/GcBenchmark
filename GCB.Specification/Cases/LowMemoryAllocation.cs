73451a50e08251a4de141d645210c425b1163b86
dev@mikovilla.com <dev@mikovilla.com>
2024-10-20 13:20:31 -0700

    added low memory allocation case

---
+++ GCB.Specification/Cases/LowMemoryAllocation.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace GCB.Specification.Cases
+{
+    [MemoryDiagnoser]
+    public class LowMemoryAllocation
+    {
+        private List<byte[]> _data = new List<byte[]>();
+
+        [Benchmark]
+        public void AllocateMemory()
+        {
+            for (int i = 0; i < 10_000; i++)
+            {
+                _data.Add(new byte[1_000]); // Allocate 1 KB arrays
+            }
+        }
+
+        [Benchmark]
+        public void ProcessData()
+        {
+            int sum = 0;
+            for (int i = 0; i < _data.Count; i++)
+            {
+                sum += _data[i][0]; // Simulate some processing
+            }
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            _data.Clear();
+        }
+    }
+}
